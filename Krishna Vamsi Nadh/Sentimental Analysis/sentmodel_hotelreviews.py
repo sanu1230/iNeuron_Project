17123adb24121bc9a877d0417db0be440a90ea2b
KRISHNA VAMSI NADH <60399427+VAMSINADH2000@users.noreply.github.com>
2021-01-17 09:00:06 +0530

    Update sentmodel_hotelreviews.py

--- Krishna Vamsi Nadh/Sentimental Analysis/sentmodel_hotelreviews.py
+++ Krishna Vamsi Nadh/Sentimental Analysis/sentmodel_hotelreviews.py
@@ -96,6 +96,21 @@ metrics(predictions, y_test)
 
 
 #-----------------------------------------------------------------------
-model = pickle.load(open('model_hotelreviews.pkl', 'rb'))
-
-print(predict(model,"bad"))
+def predict(review):
+    model = pickle.load(open('model_hotelreviews.pkl', 'rb'))
+    l = []
+    l.append(review)
+    sent = model.predict(l)
+    if sent[0]== 0:
+        return "Negative"
+    else:
+        return "Positive"
+customer_reviews = ['Siddharth from MyLoanCare helped well and the loan got processed fast','Its a very good experience with you. Here I found the best deal and best interest rate of all banks'
+                   ,'Very good. Special thanks to Saurabh Gandhi from MyLoanCare and Ram from HDFC Bank',
+                   'Amazing service. Really happy with the team. Definitely recommend it to everyone.',
+                   'Worst service by the bank',
+                   'had very bad expierience']
+for i in customer_reviews:
+    print(predict(i))
+    
+    
